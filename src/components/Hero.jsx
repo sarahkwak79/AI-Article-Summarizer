7f497a6925328acfa486666cdd5922c3fd18d9d6
Sarah Kwak <sarahkwak@Sarahs-MacBook-Pro.local>
2023-12-31 04:27:28 -0500

    Initial commit

---
+++ src/components/Hero.jsx
@@ -0,0 +1,33 @@
+import { logo } from "../assets";
+
+const Hero = () => {
+  return (
+    <header className="w-full flex justify-center items-center flex-col">
+      <nav className="flex justify-between items-center w-full mb-10 pt-3">
+        <img src={logo} alt="sumz_logo" className="w-28 object-contain" />
+
+        <button
+          type="button"
+          onClick={() =>
+            window.open(
+              "https://www.goodhousekeeping.com/life/pets/g4531/cutest-dog-breeds/"
+            )
+          }
+          className="black_btn"
+        >
+          GitHub
+        </button>
+      </nav>
+
+      <h1 className="head_text">
+        Summarize Articles with <br className="max-mdLhidden" />
+        <span className="orange_gradient">OpenAI GPT-4</span>
+      </h1>
+      <h2 className="desc">
+          Simplify your reading with AI-Summy, an open-source article summarizer that transforms lengthy articles into clear and concise summaries
+      </h2>
+    </header>
+  );
+};
+
+export default Hero;
